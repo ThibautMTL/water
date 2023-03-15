1b5a34bf2a722ee8401b61c24c38bf7ac7e621f6
Thibaut <thibaut@tractr.net>
2023-03-15 09:41:20 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'water-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'water';
+}
